3181406f45d480bf98ff8fee5e0b8a865b4e4189
STBrian <brichap100@gmail.com>
2024-01-08 21:32:00 -0600

    Update batch file

--- build.bat
+++ build.bat
@@ -1,4 +1,12 @@
 @ECHO OFF
+if exist build\ (
+    del /S /Q build\\*
+    rmdir /S /Q build
+)
+if exist dist\ (
+    del /S /Q dist\\*
+    rmdir /S /Q dist
+)
 pyinstaller mc3ds-tm.spec
 pyinstaller mc3ds-tm-gui.spec
 pause
