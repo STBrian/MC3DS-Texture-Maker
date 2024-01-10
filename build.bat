2db49d2fb0630d77255491a98064678c6d6ac58a
STBrian <brichap100@gmail.com>
2024-01-10 16:10:48 -0600

    Update windows build script

--- build.bat
+++ build.bat
@@ -7,6 +7,7 @@ if exist dist\ (
     del /S /Q dist\\*
     rmdir /S /Q dist
 )
-pyinstaller mc3ds-tm.spec
+pyinstaller --onefile --icon icon.ico --add-data assets:assets --add-data modules:modules --add-data icon.ico:. --add-data icon.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
 pyinstaller --onefile --hide-console hide-early --icon icon.ico --add-data assets:assets --add-data modules:modules --add-data icon.ico:. --add-data icon.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
+del mc3ds-tm.spec
 del mc3ds-tm-gui.spec
