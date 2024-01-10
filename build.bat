ee62f3eca0531e7594e7215fcc81e386642efaaf
STBrian <brichap100@gmail.com>
2024-01-10 13:51:10 -0600

    Del spec file

--- build.bat
+++ build.bat
@@ -9,4 +9,4 @@ if exist dist\ (
 )
 pyinstaller mc3ds-tm.spec
 pyinstaller --onefile --hide-console hide-early --icon icon.ico --add-data assets:assets --add-data modules:modules --add-data icon.ico:. --add-data icon.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
-pause
+del mc3ds-tm-gui.spec
