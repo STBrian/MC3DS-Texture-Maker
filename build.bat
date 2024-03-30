1fc810628f69e8b06cca588c8c722d6a46722601
STBrian <brichap100@gmail.com>
2024-03-29 20:30:32 -0600

    Updated build scripts and workflow

--- build.bat
+++ build.bat
@@ -7,7 +7,5 @@ if exist dist\ (
     del /S /Q dist\\*
     rmdir /S /Q dist
 )
-pyinstaller --onefile --icon icon.ico --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm.py
 pyinstaller --onefile --hide-console hide-early --icon icon.ico --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
-del mc3ds-tm.spec
 del mc3ds-tm-gui.spec
--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ then
 rm -r ./dist
 fi
 
-python3.11 -m PyInstaller --onefile --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm.py
-python3.11 -m PyInstaller --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
+python3.12 -m PyInstaller --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
 
 rm mc3ds-tm.spec
 rm mc3ds-tm-gui.spec
