412a01955a8d01a564f1a836e4f6b2c1b6100686
STBrian <brichap100@gmail.com>
2024-05-10 14:29:09 -0600

    Added tool for viewing mc3ds supported 3dst textures

---
+++ 3dstViewer.py
@@ -0,0 +1,54 @@
+import argparse, os, sys, customtkinter
+from tkinter import messagebox
+from PIL import Image
+from pathlib import Path
+from modules.tex3dst import *
+
+class App(customtkinter.CTk):
+    def __init__(self, title: str, image: Image.Image):
+        super().__init__()
+
+        minwidth = 256
+        minheight = 256
+        if image.size[0] < minwidth:
+            width = minwidth
+        else:
+            width = image.size[0]
+
+        if image.size[1] < minheight:
+            height = minheight
+        else:
+            height = image.size[1]
+
+        self.title(f"{title} - 3dstViewer")
+        self.geometry(f"{width}x{height}")
+        self.resizable(False, False)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.configure(fg_color="black")
+
+        portviewImg = customtkinter.CTkImage(light_image=image, dark_image=image, size=(image.size[0], image.size[1]))
+        portviewFrame = customtkinter.CTkLabel(self, image=portviewImg, text="", compound="top", bg_color="black")
+        portviewFrame.grid(row=0, column=0, padx=0, pady=0)
+
+if len(sys.argv) < 2:
+    messagebox.showerror(title="Error", message="No file provided")
+    sys.exit()
+
+parser = argparse.ArgumentParser(description='A 3dst texture file viewer')
+parser.add_argument('path', type=str, help="The path of the file")
+args = parser.parse_args()
+
+if os.path.exists(args.path):
+    path = Path(args.path)
+    try:
+        texture = Texture3dst().open(path)
+    except:
+        messagebox.showerror(title="Error - 3dstViewer", message="Incompatible file")
+        sys.exit()
+    texture.flipX()
+    preview = texture.copy(0, 0, texture.width, texture.height)
+
+    customtkinter.set_appearance_mode("dark")
+    app = App(path.name, preview)
+    app.mainloop()
--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ rm -r ./dist
 fi
 
 python3.12 -m PyInstaller --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3ds-tm-gui.py
-
-rm mc3ds-tm.spec
+python3.12 -m PyInstaller --onefile --windowed --icon icon.ico --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder 3dstViewer.py
 rm mc3ds-tm-gui.spec
+rm 3dstViewer.spec
