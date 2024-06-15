9c4dd71e688ec278f45bfab142533720538347c2
STBrian <brichap100@gmail.com>
2024-06-15 17:35:49 -0600

    Change output exe file names

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ then
 rm -r ./dist
 fi
 
-python3.12 -m PyInstaller --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3dstm.py
-python3.12 -m PyInstaller --onefile --windowed --icon icon.ico --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder py3dstViewer.py
-rm mc3ds-tm.spec
-rm 3dstViewer.spec
+python3.12 -m PyInstaller --name=MC3DS-Texture-Maker --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder mc3dstm.py
+python3.12 -m PyInstaller --name=3DST-Viewer --onefile --windowed --icon icon.ico --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder py3dstViewer.py
+rm MC3DS-Texture-Maker.spec
+rm 3DST-Viewer.spec
