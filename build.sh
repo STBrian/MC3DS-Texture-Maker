c59c6803960264500dbc825d4443438e3cb80168
STBrian <brichap100@gmail.com>
2024-08-31 16:48:29 -0600

    Renamed from mc3dstm.py -> main.py

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ then
 rm -r ./dist
 fi
 
-python3.12 -m PyInstaller --name=MC3DS-Texture-Maker --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder --hidden-import pkg_resources.extern mc3dstm.py
+python3.12 -m PyInstaller --name=MC3DS-Texture-Maker --onefile --hide-console hide-early --add-data assets:assets --add-data icon.ico:. --add-data icon.png:. --add-data icon2.png:. --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder --hidden-import pkg_resources.extern main.py
 python3.12 -m PyInstaller --name=3DST-Viewer --onefile --windowed --icon icon.ico --hidden-import PIL --hidden-import PIL._imagingtk --hidden-import PIL._tkinter_finder --hidden-import pkg_resources.extern py3dstViewer.py
 rm MC3DS-Texture-Maker.spec
 rm 3DST-Viewer.spec
