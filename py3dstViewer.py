55b04ae66148deefc10e88d66fce5eda977e5a4f
STBrian <brichap100@gmail.com>
2024-12-20 17:43:08 -0600

    Added feature to show mipmaps #9

--- py3dstViewer.py
+++ py3dstViewer.py
@@ -1,12 +1,30 @@
 import argparse, os, sys, customtkinter
 import traceback
 import CTkMenuBar
+
+from modules.MyCTkTopLevel import *
 from tkinter import messagebox
 from PIL import Image, ImageTk, ImageDraw
 from pathlib import Path
 from py3dst import Texture3dst, Texture3dstUnsupported, Texture3dstNoSignature
+from py3dst.tex3dst import _createPixelDataStructure, _getTexturePosition
+from py3dst.error_classes import Texture3dstUnexpectedEndOfFile
 
 VERSION = "0.6.1"
+
+def _generateChessboardPattern(width, height, tileSize = 10):
+    chessboard = Image.new("RGBA", (width, height), (180, 180, 180, 255))
+    draw = ImageDraw.Draw(chessboard)
+
+    for y in range(0, height, tileSize):
+        for x in range(0, width, tileSize):
+            if (x // tileSize + y // tileSize) % 2 == 0:
+                draw.rectangle(
+                    [(x, y), (x + tileSize - 1, y + tileSize - 1)], 
+                    fill=(128, 128, 128, 255)
+                )
+    
+    return chessboard
 
 class App(customtkinter.CTk):
     def __init__(self, imgPath: Path|None):
@@ -47,6 +65,9 @@ class App(customtkinter.CTk):
         fileMenu.add_separator()
         fileMenu.add_option("Exit", command=self.closeApp)
 
+        viewMenu = CTkMenuBar.CustomDropdownMenu(widget=menu_bar.add_cascade("View"))
+        viewMenu.add_option("Show mipmaps", command=self.showMipmaps)
+
         helpMenu = CTkMenuBar.CustomDropdownMenu(widget=menu_bar.add_cascade("Help"))
         helpMenu.add_option("About", command=self.showAbout)
 
@@ -83,6 +104,14 @@ class App(customtkinter.CTk):
         formatField = customtkinter.CTkEntry(infoFrame, state="readonly", textvariable=formatValue)
         formatField.grid(column=0, row=4, sticky="we", padx=10)
 
+        mipLevelLabel = customtkinter.CTkLabel(infoFrame, text="Mipmap levels", width=200, font=(None, 12), anchor="w")
+        mipLevelLabel.grid(column=0, row=5, sticky="we", padx=10, pady=(10,0))
+
+        mipLevelValue = customtkinter.StringVar()
+        self.mipLevelValue = mipLevelValue
+        mipLevelField = customtkinter.CTkEntry(infoFrame, state="readonly", textvariable=mipLevelValue)
+        mipLevelField.grid(column=0, row=6, sticky="we", padx=10)
+
         if imgPath != None:
             self.openPath(imgPath.absolute())
         else:
@@ -100,24 +129,26 @@ class App(customtkinter.CTk):
 
         self.sizeValue.set("")
         self.formatValue.set("")
+        self.mipLevelValue.set("")
 
     def exportAs(self):
-        supportedExtensions = Image.registered_extensions()
-        saveSupportedExtensions = {ex for ex, f in supportedExtensions.items() if f in Image.SAVE}
-        saveSuppExtStr = ""
-        for ex in saveSupportedExtensions:
-            saveSuppExtStr += f"{ex} "
-        filePath = customtkinter.filedialog.asksaveasfilename(filetypes=[("Image files", ".png .jpeg .jpg .webp .bmp"), ("All image files", saveSuppExtStr)])
-        if filePath != '':
-            texture = Texture3dst().open(self.imgPath)
-            imgCopy = texture.copy(0, 0, texture.size[0], texture.size[1])
-            try:
-                imgCopy.save(filePath)
-            except ValueError:
+        if self.imgOpen:
+            supportedExtensions = Image.registered_extensions()
+            saveSupportedExtensions = {ex for ex, f in supportedExtensions.items() if f in Image.SAVE}
+            saveSuppExtStr = ""
+            for ex in saveSupportedExtensions:
+                saveSuppExtStr += f"{ex} "
+            filePath = customtkinter.filedialog.asksaveasfilename(filetypes=[("Image files", ".png .jpeg .jpg .webp .bmp"), ("All image files", saveSuppExtStr)])
+            if filePath != '':
+                texture = Texture3dst().open(self.imgPath)
+                imgCopy = texture.copy(0, 0, texture.size[0], texture.size[1])
                 try:
-                    imgCopy.save(f"{filePath}.png")
-                except:
-                    messagebox.showerror("Error - 3DSTViewer", "An exception occurred while exporting the texture")
+                    imgCopy.save(filePath)
+                except ValueError:
+                    try:
+                        imgCopy.save(f"{filePath}.png")
+                    except:
+                        messagebox.showerror("Error - 3DSTViewer", "An exception occurred while exporting the texture")
 
     def openFile(self):
         filePath = customtkinter.filedialog.askopenfilename(filetypes=[("3DST Texture", ".3dst")])
@@ -144,18 +175,8 @@ class App(customtkinter.CTk):
                         height = preview.size[1]
                     self.geometry(f"{width+250}x{height+30}")
 
-                    chessboard = Image.new("RGBA", (texture.size[0], texture.size[1]), (180, 180, 180, 255))
-                    draw = ImageDraw.Draw(chessboard)
-
-                    tilesize = 10
-                    for y in range(0, texture.size[1], tilesize):
-                        for x in range(0, texture.size[0], tilesize):
-                            if (x // tilesize + y // tilesize) % 2 == 0:
-                                draw.rectangle(
-                                    [(x, y), (x + tilesize - 1, y + tilesize - 1)], 
-                                    fill=(128, 128, 128, 255)
-                                )
-                    
+                    # Generates the chessboard pattern in a secondary image then fuses with the source image
+                    chessboard = _generateChessboardPattern(texture.size[0], texture.size[1])
                     previewFinal = Image.alpha_composite(chessboard, preview)
 
                     self.title(f"{path.name} - 3DSTViewer")
@@ -164,6 +185,7 @@ class App(customtkinter.CTk):
 
                     self.sizeValue.set(f"{preview.size[0]} x {preview.size[1]}")
                     self.formatValue.set(f"{texture._getFormatInfo(texture.header.format)["name"].upper()}")
+                    self.mipLevelValue.set(f"{texture.header.mip_level}")
 
                     self.imgName = path.name
                     self.imgPath = path.absolute()
@@ -175,8 +197,77 @@ class App(customtkinter.CTk):
                 except:
                     messagebox.showerror(title="Error - 3DSTViewer", message=f"Unhandled error while opening file.\n{traceback.format_exc()}")
 
+    def showMipmaps(self):
+        if self.imgOpen:
+            texture = Texture3dst().open(self.imgPath)
+            if texture.header.mip_level > 1:
+                secondary_window = MyCTkTopLevel(self)
+                secondary_window.title("Mipmap viewer")
+                secondary_window.geometry(f"{int(texture.header.full_size[0]/2)}x{int(texture.header.full_size[1]/2)}")
+
+                icon = ImageTk.PhotoImage(file=os.path.join(self.app_path, "icon_viewer.png"))
+                secondary_window.wm_iconbitmap()
+                secondary_window.iconphoto(False, icon)
+
+                texPixelFormat = texture._getFormatInfo(texture.header.format)
+                texPixelLenght = texPixelFormat["pixel_lenght"]
+                
+                start_idx = 0x20
+                mipData_end_idx = start_idx + texPixelLenght * texture.header.full_size[0] * texture.header.full_size[1]
+                
+                mipmapsFrames = 0
+                for i in range(0, texture.header.mip_level - 1):
+                    miptexWidth = int(texture.header.full_size[0] / (2 * (i+1)))
+                    miptexHeight = int(texture.header.full_size[1] / (2 * (i+1)))
+                    mipData_start_idx = mipData_end_idx 
+                    mipData_end_idx = mipData_start_idx + (texPixelLenght * miptexWidth * miptexHeight)
+
+                    unarranged_pixelData = _createPixelDataStructure(miptexWidth, miptexHeight, texPixelLenght)
+                    with open(self.imgPath, "rb") as f:
+                        f.seek(mipData_start_idx)
+                        for j in range(miptexHeight):
+                            for k in range(miptexWidth):
+                                pixelRead = f.read(texPixelLenght)
+                                if not pixelRead:
+                                    raise Texture3dstUnexpectedEndOfFile
+                                unarranged_pixelData[j][k] = pixelRead
+                    
+                    textureData = _createPixelDataStructure(miptexWidth, miptexHeight, texPixelLenght)
+                    for j in range(miptexHeight):
+                        for k in range(miptexWidth):
+                            dst_pos = _getTexturePosition(k, j, miptexWidth)
+                            textureData[j][k] = unarranged_pixelData[dst_pos[1]][dst_pos[0]]
+
+                    textureData.reverse()
+
+                    tmpTexture = Texture3dst().new(miptexWidth, miptexHeight, format=texPixelFormat["name"])
+                    for j in range(miptexHeight):
+                        for k in range(miptexWidth):
+                            pixel_values = tmpTexture._convertBytesToPixelData(textureData[j][k])
+                            tmpTexture.setPixel(k, j, pixel_values)
+
+                    preview = _generateChessboardPattern(miptexWidth, miptexHeight)
+                    preview = Image.alpha_composite(preview, tmpTexture.copy(0, 0, tmpTexture.size[0], tmpTexture.size[1]))
+
+                    portviewFrame = customtkinter.CTkLabel(secondary_window, width=miptexWidth, height=miptexHeight, text="", compound="top", bg_color="black")
+                    portviewFrame.grid(column=i, row=0, sticky="n")
+                    
+                    portviewImg = customtkinter.CTkImage(light_image=preview, dark_image=preview, size=(miptexWidth, miptexHeight))
+                    portviewFrame.configure(image=portviewImg)
+                    
+                    mipmapsFrames += 1
+                
+                finalWidth = 0
+                finalHeight = int(texture.header.full_size[1] / 2)
+                for i in range(mipmapsFrames):
+                    finalWidth += texture.header.full_size[0] / (2 * (i+1))
+
+                secondary_window.geometry(f"{int(finalWidth)}x{finalHeight}")
+            else:
+                messagebox.showinfo("No mipmaps available", "Texture does not contain mipmap data\nMipmap level is 1")
+
     def showAbout(self):
-        about_text = f"3DST Viewer\nVersion {self.version}\n\nMade by: STBrian\nGitHub: https://github.com/STBrian"
+        about_text = f"3DSTViewer\nVersion {self.version}\n\nMade by: STBrian\nGitHub: https://github.com/STBrian"
         messagebox.showinfo("About", about_text)
 
     def closeApp(self):
